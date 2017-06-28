f766210892ba57ce98e3272bfd4b9cbbc7957ba5
13949836783@163.com <13949836783@163.com>
2017-06-28 21:53:58 +0800

    generate random website for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   validates_presence_of :billing_name, :billing_address, :shipping_name, :shipping_address
   has_many :product_lists
   belongs_to :user
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
 end
